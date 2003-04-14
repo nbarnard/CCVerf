8bc2c3970613e344bcd6896b635586e37cee71ed
Nicholas Barnard <github-298f98981b@nickb.users.panix.com>
2003-04-14 00:00:00 -0500

    Got rid of silly uses of sprintf now uses casting out nines and the modulus operator and released under the LGPL

--- ccverf.c
+++ ccverf.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 #include<ctype.h>
 
 /***************************************************************************
  * Function     : ccverf
  * Author       : Nicholas Clifton Barnard <nickb@pobox.com>
- * Date         : February 5, 1997
+ * Date         : April 14, 2003
  * Description  : Validates a Credit Card Number
  * Arguments    : char card -  credit card number - Tolerant of Non-Numbers
  * int type - type of credit card
@@ -16,16 +15,30 @@
  * 4 - Discover 
  * Return Value : integer - 0 - invalid card number
  *                          1 - valid card number
- * Copyright    : Copyright (C) 1996 Nicholas Barnard.  All Rights Reserved.
- * Anyone may use this function within their programs as long as my
- * name and E-Mail address are contained within the documentation in the
- * same font and size as the main body of the documentation.  An E-Mail
- * is also requested if you use this function within your program.
- * You may modify this function, as long as you credit yourself
- * with your name and e-mail address.  You must also send a copy
- * of your modified version to me.
+ * Update notes : 4/14/2003 - Got rid of silly uses of sprintf now uses casting
+ *                            out nines and the modulus operator and released 
+ *                            under the LGPL
  * Special Thanks to Dave Paris <spider@servtech.com> for help with the
  * Mod 10 Algorithm.
+ *
+ * An E-Mail is requested if you use this function within your program.  It is
+ * also requested that you send a copy of modified version to me.
+ *
+ * Copyright (C) 1996,2003 Nicholas Clifton Barnard
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *************************************************************************/
 
 int
@@ -33,7 +46,6 @@ ccverf(char *card, int type)
 {
 	int ccnumber[16];
 	int final = 0;
-	char temp[256];
 	int digits = 0;
 	int counter;
 	int len;
@@ -110,11 +122,10 @@ ccverf(char *card, int type)
 
 		/*
 		 * Performs single digit doubling on ccnumber[counter] i.e.
-		 * 8 doubled into 7 (8 * 2 = 16 1 + 6 = 7)
+		 * 8 doubled into 7 (8 * 2 = 16 1 + 6 = 7) by casting out nines
 		 */
 		if (twice >= 10) {
-			sprintf(temp, "%d", twice);
-			ccnumber[counter] = (((temp[0]) - 48) + ((temp[1]) - 48));
+			ccnumber[counter] = twice-9;                        
 		} else {
 			ccnumber[counter] = twice;
 		}
@@ -125,8 +136,8 @@ ccverf(char *card, int type)
 	}
 
 	/* See if the final digit is a zero, if so the card is valid. */
-	sprintf(temp, "%d", final);
-	if ((temp[(strlen(temp) - 1)]) != '0') {
+	
+        if ((final%10)!=0) {
 		return (0);
 	} else {
 		return (1);
